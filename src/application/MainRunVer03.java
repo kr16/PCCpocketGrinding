6861c6ceacd3cc180139592c7d73eca957a40afd
mpiotrowski <mpiotrowski@WIN-1A0QF0CCDD1>
2016-12-12 16:03:44 -0500

    --- automatic rebase to remote; old local SHA: d80303c3cd58d3ea45148ecdd9ceda7760660133 ---

--- src/application/MainRunVer03.java
+++ src/application/MainRunVer03.java
@@ -188,8 +188,8 @@ public class MainRunVer03 extends RoboticsAPIApplication {
 	}
 	public void scanCycle() {
 		
-		for (int row = 1; row < coupon.getRowCount(); row++) {
-			for (int column = 1; column < coupon.getColumnCount(); column++) {
+		for (int row = 1; row <= coupon.getRowCount(); row++) {
+			for (int column = 1; column <= coupon.getColumnCount(); column++) {
 				if ((coupon.getRowColumnValue(row, column) != EHotDotCouponStates.Skip) 
 						&& (coupon.getRowColumnValue(row, column) != EHotDotCouponStates.Smudged)) {
 					emptyScanCycle(row, column);
