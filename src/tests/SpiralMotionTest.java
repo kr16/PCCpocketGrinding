cbf0311c469e2bee9894b7686dc8a7f6bbc1d3e1
mpiotrowski <mpiotrowski@WIN-1A0QF0CCDD1>
2016-12-14 11:32:27 -0500

    --- automatic rebase to remote; old local SHA: 8bf1f0274b81526862dae823b31169ee97ac0338 ---

--- src/tests/SpiralMotionTest.java
+++ src/tests/SpiralMotionTest.java
@@ -61,7 +61,8 @@ public class SpiralMotionTest extends RoboticsAPIApplication {
 		HotDotTest.attachTo(bot.getFlange());
 		CartesianSineImpedanceControlMode spiralMode;
 		CartesianImpedanceControlMode mode = new CartesianImpedanceControlMode();
-		double totalTimeSec = 300;
+		double totalTimeSec = 300;	//	s
+		double frequency = 2;		//	Hz
 		
 		//bot home
 		System.out.println("Moving to Home/Start position");
@@ -70,7 +71,7 @@ public class SpiralMotionTest extends RoboticsAPIApplication {
 		TouchForceRecord hitTable = new TouchForceRecord();
 		hitTable.recordPosition(searchDir.PosX, 5, 30, 20, 0, currentTCP, nullBase, bot);
 		
-		spiralMode = CartesianSineImpedanceControlMode.createSpiralPattern(CartPlane.YZ,3, 25, 3000, totalTimeSec);
+		spiralMode = CartesianSineImpedanceControlMode.createSpiralPattern(CartPlane.YZ,frequency, 25, 3000, totalTimeSec);
 		spiralMode.parametrize(CartDOF.X).setBias(10).setStiffness(3000);
 		//currentTCP.move(linRel(100, 0, 0, nullBase).setCartVelocity(1).setMode(spiralMode));
 		currentTCP.move(positionHold(spiralMode, (long)totalTimeSec, TimeUnit.SECONDS));
