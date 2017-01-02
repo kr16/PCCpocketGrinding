98d1a00bd196de818c8a76f52236a9991d4f3432
mpiotrowski <mpiotrowski@WIN-1A0QF0CCDD1>
2017-01-02 15:08:39 -0500

    --- automatic rebase to remote; old local SHA: e47901a4ac913770cef067a02937257e2e2b72c6 ---

--- src/com/kuka/generated/ioAccess/CognexProfinetIOGroup.java
+++ src/com/kuka/generated/ioAccess/CognexProfinetIOGroup.java
@@ -32,6 +32,15 @@ public class CognexProfinetIOGroup extends AbstractIOGroup
 		addDigitalOutput("AcqCtrlReg0", IOTypes.BOOLEAN, 1);
 		addDigitalOutput("AcqCtrlReg1", IOTypes.BOOLEAN, 1);
 		addDigitalOutput("AcqCtrlReg7", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg0", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg1", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg2", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg3", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg4", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg5", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg6", IOTypes.BOOLEAN, 1);
+		addInput("AcqStatusReg7", IOTypes.BOOLEAN, 1);
+		addInput("AcqCount", IOTypes.UNSIGNED_INTEGER, 16);
 	}
 
 	/**
@@ -155,6 +164,186 @@ public class CognexProfinetIOGroup extends AbstractIOGroup
 	public void setAcqCtrlReg7(java.lang.Boolean value)
 	{
 		setDigitalOutput("AcqCtrlReg7", value);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg0</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg0'
+	 */
+	public boolean getAcqStatusReg0()
+	{
+		return getBooleanIOValue("AcqStatusReg0", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg1</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg1'
+	 */
+	public boolean getAcqStatusReg1()
+	{
+		return getBooleanIOValue("AcqStatusReg1", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg2</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg2'
+	 */
+	public boolean getAcqStatusReg2()
+	{
+		return getBooleanIOValue("AcqStatusReg2", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg3</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg3'
+	 */
+	public boolean getAcqStatusReg3()
+	{
+		return getBooleanIOValue("AcqStatusReg3", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg4</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg4'
+	 */
+	public boolean getAcqStatusReg4()
+	{
+		return getBooleanIOValue("AcqStatusReg4", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg5</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg5'
+	 */
+	public boolean getAcqStatusReg5()
+	{
+		return getBooleanIOValue("AcqStatusReg5", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg6</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg6'
+	 */
+	public boolean getAcqStatusReg6()
+	{
+		return getBooleanIOValue("AcqStatusReg6", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqStatusReg7</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'AcqStatusReg7'
+	 */
+	public boolean getAcqStatusReg7()
+	{
+		return getBooleanIOValue("AcqStatusReg7", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>AcqCount</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [0; 65535]
+	 *
+	 * @return current value of the digital input 'AcqCount'
+	 */
+	public java.lang.Integer getAcqCount()
+	{
+		return getNumberIOValue("AcqCount", false).intValue();
 	}
 
 }
