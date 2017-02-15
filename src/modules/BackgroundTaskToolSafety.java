f21e690c5dfa478bc0be9ca58d67a0d5ea161752
mpiotrowski <mpiotrowski@WIN-1A0QF0CCDD1>
2017-02-15 16:39:46 -0500

    --- automatic rebase to remote; old local SHA: c061f7ee5883989991aeadd577a82ac3df71d4f4 ---

---
+++ src/modules/BackgroundTaskToolSafety.java
@@ -0,0 +1,49 @@
+package modules;
+
+
+import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+import com.kuka.roboticsAPI.applicationModel.tasks.CycleBehavior;
+import com.kuka.roboticsAPI.applicationModel.tasks.RoboticsAPICyclicBackgroundTask;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.controllerModel.sunrise.ISafetyState;
+import com.kuka.roboticsAPI.controllerModel.sunrise.SunriseSafetyState.EnablingDeviceState;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+
+/**
+ * Implementation of a cyclic background task.
+ * <p>
+ * It provides the {@link RoboticsAPICyclicBackgroundTask#runCyclic} method 
+ * which will be called cyclically with the specified period.<br>
+ * Cycle period and initial delay can be set by calling 
+ * {@link RoboticsAPICyclicBackgroundTask#initializeCyclic} method in the 
+ * {@link RoboticsAPIBackgroundTask#initialize()} method of the inheriting 
+ * class.<br>
+ * The cyclic background task can be terminated via 
+ * {@link RoboticsAPICyclicBackgroundTask#getCyclicFuture()#cancel()} method or 
+ * stopping of the task.
+ * @see UseRoboticsAPIContext
+ * 
+ */
+public class BackgroundTaskToolSafety extends RoboticsAPICyclicBackgroundTask {
+	@Inject
+	Controller kUKA_Sunrise_Cabinet_1;
+	LBR botState;
+	ISafetyState currentState; 
+	GrindingTool btTool;
+
+	@Override
+	public void initialize() {
+		currentState = botState.getSafetyState();
+		btTool = new GrindingTool(kUKA_Sunrise_Cabinet_1);
+		initializeCyclic(0, 500, TimeUnit.MILLISECONDS,
+				CycleBehavior.BestEffort);
+	}
+
+	@Override
+	public void runCyclic() {
+		if (currentState.getEnablingDeviceState() == EnablingDeviceState.NONE) {
+			btTool.grindingStop();
+		}
+	}
+}
