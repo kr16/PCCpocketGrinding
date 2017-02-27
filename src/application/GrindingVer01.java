e5c5c1473198e22a3ca5850819fb8457befc5540
mpiotrowski <mpiotrowski@WIN-1A0QF0CCDD1>
2017-02-27 15:16:21 -0500

    --- automatic rebase to remote; old local SHA: c03750028655abc92a653df4b0bff20723fc8746 ---

--- src/application/GrindingVer01.java
+++ src/application/GrindingVer01.java
@@ -125,7 +125,7 @@ public class GrindingVer01 extends RoboticsAPIApplication {
 		currentTCP = eeTool.setCurrentTCP(EToolName.BallWorking);
 		
 		System.out.println("START");
-		currentTCP.move(linRel(20, 0, 0, currentTCP).setCartVelocity(1).setCartAcceleration(200));
+		currentTCP.move(linRel(20, 0, 0, currentTCP).setCartVelocity(20).setCartAcceleration(200));
 		System.out.println("STOP");
 		
 		getApplicationControl().halt();
