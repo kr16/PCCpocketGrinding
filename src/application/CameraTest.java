c5d775fe28ab5d884f2ecb65d5b3e2cd90456727
kr16 <slayerowicz@gmail.com>
2017-04-27 10:20:28 -0400

    --- automatic rebase to remote; old local SHA: 1c56ade948bca92d2c7d38324e22ebbb898de3c0 ---

--- src/application/CameraTest.java
+++ src/application/CameraTest.java
@@ -1,8 +1,20 @@
 package application;
 
 
+import java.awt.Button;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.print.PrinterJob;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 
 import modules.TimerKCT;
@@ -61,6 +73,85 @@ public class CameraTest extends RoboticsAPIApplication {
 	public void run() {
 		// your application execution starts here
 		//bot.move(ptpHome());
+		// Set system property.
+        // Call this BEFORE the toolkit has been initialized, that is,
+        // before Toolkit.getDefaultToolkit() has been called.
+        System.setProperty("java.awt.headless", "true");
+
+        // This triggers creation of the toolkit.
+        // Because java.awt.headless property is set to true, this 
+        // will be an instance of headless toolkit.
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        // Standard beep is available.
+        tk.beep();
+
+        // Check whether the application is
+        // running in headless mode.
+        GraphicsEnvironment ge = 
+        GraphicsEnvironment.getLocalGraphicsEnvironment();
+        System.out.println("Headless mode: " + ge.isHeadless());
+
+        // No top levels are allowed.
+        boolean created = false;
+        try
+        {
+            Frame f = new Frame("Frame");
+            created = true;
+        }
+        catch (Exception z)
+        {
+            z.printStackTrace(System.err);
+            created = false;
+        }
+        System.err.println("Frame is created: " + created);
+
+        // No other components except Canvas and Panel are allowed.
+        created = false;
+        try
+        {
+            Button b = new Button("Button");
+            created = true;
+        }
+        catch (Exception z)
+        {
+            z.printStackTrace(System.err);
+            created = false;
+        }
+        System.err.println("Button is created: " + created);
+        
+        // Canvases can be created.
+        final Canvas c = new Canvas()
+        {
+            public void paint(Graphics g)
+            {
+                Rectangle r = getBounds();
+                g.drawLine(0, 0, r.width - 1, r.height - 1);
+                // Colors work too.
+                g.setColor(new Color(255, 127, 0));
+                g.drawLine(0, r.height - 1, r.width - 1, 0);
+                // And fonts
+                g.setFont(new Font("Arial", Font.ITALIC, 12));
+                g.drawString("Test", 32, 8);
+            }
+        };
+        // And all the operations work correctly.
+        c.setBounds(32, 32, 128, 128);
+
+        // Images are available.
+        Image i = null;
+        try
+        {
+            File f = new File("grapefruit.jpg");
+            i = ImageIO.read(f);
+        }
+        catch (Exception z)
+        {
+            z.printStackTrace(System.err);
+        }
+        final Image im = i;
+        
+        
+		
 		System.setProperty("java.awt.headless", "false");
 		System.out.println(GraphicsEnvironment.isHeadless());
 		JOptionPane.showMessageDialog(null, "i pozamiatane...", "Hole misiek!", JOptionPane.WARNING_MESSAGE);
