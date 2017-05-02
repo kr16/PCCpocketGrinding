dabf882b6044f64c46dc81c1d38f15f090446e87
kr16 <slayerowicz@gmail.com>
2017-05-02 15:44:27 -0400

    --- automatic rebase to remote; old local SHA: 7acb7a7eca259dabbf1e3b262fd3fd199e8f06d9 ---

--- src/application/LucanaCollectPictures.java
+++ src/application/LucanaCollectPictures.java
@@ -111,8 +111,8 @@ public class LucanaCollectPictures extends RoboticsAPIApplication {
 		
 		telnet.write("hAuto"+"\n");
 		//telnet.readLucanaResponse(false);
-		//buffer = telnet.readLucanaResponseDumpBytes(true);
-		lucanaData.dumpLucanaDataToFile(buffer);
+		telnet.readLucanaResponseDumpBytes(true);
+		//lucanaData.dumpLucanaDataToFile(buffer);
 		telnet.disconnect();
 		
 		getApplicationControl().halt();
