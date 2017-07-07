00e9faa25a99e30cfb327df2a41bf243ed549538
mic15wski <mic15wski@MIC0135-LT.aint.com>
2017-07-07 12:56:46 -0400

    --- automatic rebase to remote; old local SHA: d6c999b9b663ad6e92b326f79a60b65154055803 ---

--- src/application/MainRun_NoCompliance.java
+++ src/application/MainRun_NoCompliance.java
@@ -426,6 +426,7 @@ public class MainRun_NoCompliance extends RoboticsAPIApplication {
 		smudgeMode = CartesianSineImpedanceControlMode.createDesiredForce(CartDOF.X, smudgeXforce, 5000);
 		dynamicTCP.move(linRel(0,0,-length).setCartVelocity(velocity).setMode(smudgeMode));
 		extForcesAtTcp.setCommand(0);
+		System.out.println("WTF");
 		System.out.println("Forces: " + extForcesAtTcp.getExtForces().toString());
 		
 	}
